df69844b0258d345b5faefe7e2f971ab4242a38f
Unknown <kirillastakhovinbo0117@gmail.com>
2020-11-14 14:29:58 +0300

    Try

--- program.py
+++ program.py
@@ -8,3 +8,7 @@ def hello_world():
 @app.route('/v2')
 def v2():
     return 'Second action'
+
+@app.route('/astakhov')
+def astakhov():
+    return 'Hello from CI with GitHub Actions by Astakhov'
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,3 +10,8 @@ def test_v2(app, client):
     res = client.get('/v2')
     assert res.status_code == 200
     assert res.data == b"Second action"
+
+def test_astakhov(app, client):
+    res = client.get('/astakhov')
+    assert res.status_code == 200
+    assert res.data == b"Hello from CI with GitHub Actions by Astakhov"
